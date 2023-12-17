a39d568d1bf63c1ade6d8a76730ff619f14f93c6
AlexanderPoliser <alexanderpoliser@gmail.com>
2023-12-17 13:05:50 -0300

    feature(*): adding a mobile friendly menu for page navigation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: 'navbar.component.html',
   styleUrls: ['navbar.component.scss'],
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
